b6ab442c53e261e1524d76cd18aa932828a045e3
Grace Pu <33011813+Goldengold123@users.noreply.github.com>
2023-03-23 14:01:54 -0400

    Update GameShow.java
    General game works I think

--- GameShow.java
+++ GameShow.java
@@ -10,54 +10,24 @@ public class GameShow {
 
     // Variable declaration
     public static Scanner in = new Scanner(System.in);
+    public static boolean instructionsScreen = false;
+    public static boolean gameScreen = false;
+    public static String response;
 
-    // // QUestion-Answer Pair class
-    // class Pair<T> {
-    // T q, a;
-
-    // Pair() {
-
-    // }
-
-    // void make(T q, T a) {
-    // this.q = q;
-    // this.a = a;
-    // }
-
-    // Pair getP() {
-    // return this;
-    // }
-
-    // T getQ() {
-    // return this.q;
-    // }
-
-    // T getA() {
-    // return this.a;
-    // }
-    // }
-
-    // Method for clearing the screen.
-    public static void clearScreen() {
-        // OS is windows
-        // Runtime.getRuntime().exec("cls");
-
-        // 1 second delay for user to read (potential) message sent earlier
+    // Method for waiting ms milliseconds.
+    public static void wait(int ms) {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(ms);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
+    // Method for clearing the screen.
+    public static void clearScreen() {
+        wait(1000);
         // Clear screen
         System.out.println("\n".repeat(50));
-
-        // try {
-        // Runtime.getRuntime().exec("cls");
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-
     }
 
     // Method for quitting: display message then quit.
@@ -65,14 +35,22 @@ public class GameShow {
         System.out.println("-".repeat(50));
         System.out.println("Exiting...");
 
-        // 1 second delay for user to read message
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        wait(1000);
 
         System.exit(0);
+    }
+
+    // Method for reading input line that also checks if 0 is enter (return to main
+    // menu).
+    public static String readInput() throws Exception {
+        String input = in.nextLine();
+        if (input.equalsIgnoreCase("0")) {
+            instructionsScreen = false;
+            gameScreen = false;
+            throw new Exception("main menu");
+        } else
+            return input;
+
     }
 
     // Method for header text
@@ -87,207 +65,232 @@ public class GameShow {
 
     // Method for main menu
     public static void mainMenu() {
-        // Variable declaration
-        String input;
-        boolean good = false;
+        clearScreen();
 
-        while (!good) {
-            clearScreen();
+        // Game Title
+        printHeader("Are You Smarter Than A 5th Grader?");
+        // System.out.println(" █████╗ ██████╗ ███████╗ ██╗ ██╗ █████╗ ██╗
+        // ██╗\n██╔══██╗██╔══██╗██╔════╝ ╚██╗ ██╔╝██╔══██╗██║
+        // ██║\n███████║██████╔╝█████╗ ╚████╔╝ ██║ ██║██║ ██║\n██╔══██║██╔══██╗██╔══╝
+        // ╚██╔╝ ██║ ██║██║ ██║\n██║ ██║██║ ██║███████╗ ██║ ╚█████╔╝╚██████╔╝\n╚═╝
+        // ╚═╝╚═╝ ╚═╝╚══════╝ ╚═╝ ╚════╝ ╚═════╝ \n\n ██████╗███╗ ███╗ █████╗ ██████╗
+        // ████████╗███████╗██████╗ \n██╔════╝████╗
+        // ████║██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗\n ╚█████╗
+        // █╔████╔██║███████║██████╔╝ ██║ █████╗ ██████╔╝\n
+        // ╚═══██╗██║╚██╔╝██║██╔══██║██╔══██╗ ██║ ██╔══╝ ██╔══██╗\n██████╔╝██║ ╚═╝
+        // ██║██║ ██║██║ ██║ ██║ ███████╗██║ ██║\n╚═════╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═╝ ╚═╝ ╚═╝
+        // ╚══════╝╚═╝ ╚═╝\n\n████████╗██╗ ██╗ █████╗ ███╗ ██╗ █████╗
+        // ███████╗████████╗██╗ ██╗\n╚══██╔══╝██║ ██║██╔══██╗████╗ ██║ ██╔══██╗
+        // ██╔════╝╚══██╔══╝██║ ██║\n ██║ ███████║███████║██╔██╗██║ ███████║ ██████╗ ██║
+        // ███████║\n ██║ ██╔══██║██╔══██║██║╚████║ ██╔══██║ ╚════██╗ ██║ ██╔══██║\n ██║
+        // ██║ ██║██║ ██║██║ ╚███║ ██║ ██║ ██████╔╝ ██║ ██║ ██║\n ╚═╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═╝
+        // ╚══╝ ╚═╝ ╚═╝ ╚═════╝ ╚═╝ ╚═╝ ╚═╝\n\n ██████╗ ██████╗ █████╗ ██████╗
+        // ███████╗██████╗ █████╗ \n██╔════╝
+        // ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗\n██║ ██╗ ██████╔╝███████║██║
+        // ██║█████╗ ██████╔╝╚═╝███╔╝\n██║ ╚██╗██╔══██╗██╔══██║██║ ██║██╔══╝ ██╔══██╗
+        // ╚══╝ \n╚██████╔╝██║ ██║██║ ██║██████╔╝███████╗██║ ██║ ██╗ \n ╚═════╝ ╚═╝
+        // ╚═╝╚═╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═╝ ╚═╝ ");
+        // System.out.println(" ╚═════╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═╝ ╚═╝");
 
-            // Game Title
-            printHeader("Are You Smarter Than A 5th Grader?");
-            // System.out.println(" █████╗ ██████╗ ███████╗ ██╗ ██╗ █████╗ ██╗
-            // ██╗\n██╔══██╗██╔══██╗██╔════╝ ╚██╗ ██╔╝██╔══██╗██║
-            // ██║\n███████║██████╔╝█████╗ ╚████╔╝ ██║ ██║██║ ██║\n██╔══██║██╔══██╗██╔══╝
-            // ╚██╔╝ ██║ ██║██║ ██║\n██║ ██║██║ ██║███████╗ ██║ ╚█████╔╝╚██████╔╝\n╚═╝
-            // ╚═╝╚═╝ ╚═╝╚══════╝ ╚═╝ ╚════╝ ╚═════╝ \n\n ██████╗███╗ ███╗ █████╗ ██████╗
-            // ████████╗███████╗██████╗ \n██╔════╝████╗
-            // ████║██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗\n ╚█████╗
-            // █╔████╔██║███████║██████╔╝ ██║ █████╗ ██████╔╝\n
-            // ╚═══██╗██║╚██╔╝██║██╔══██║██╔══██╗ ██║ ██╔══╝ ██╔══██╗\n██████╔╝██║ ╚═╝
-            // ██║██║ ██║██║ ██║ ██║ ███████╗██║ ██║\n╚═════╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═╝ ╚═╝ ╚═╝
-            // ╚══════╝╚═╝ ╚═╝\n\n████████╗██╗ ██╗ █████╗ ███╗ ██╗ █████╗
-            // ███████╗████████╗██╗ ██╗\n╚══██╔══╝██║ ██║██╔══██╗████╗ ██║ ██╔══██╗
-            // ██╔════╝╚══██╔══╝██║ ██║\n ██║ ███████║███████║██╔██╗██║ ███████║ ██████╗ ██║
-            // ███████║\n ██║ ██╔══██║██╔══██║██║╚████║ ██╔══██║ ╚════██╗ ██║ ██╔══██║\n ██║
-            // ██║ ██║██║ ██║██║ ╚███║ ██║ ██║ ██████╔╝ ██║ ██║ ██║\n ╚═╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═╝
-            // ╚══╝ ╚═╝ ╚═╝ ╚═════╝ ╚═╝ ╚═╝ ╚═╝\n\n ██████╗ ██████╗ █████╗ ██████╗
-            // ███████╗██████╗ █████╗ \n██╔════╝
-            // ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗\n██║ ██╗ ██████╔╝███████║██║
-            // ██║█████╗ ██████╔╝╚═╝███╔╝\n██║ ╚██╗██╔══██╗██╔══██║██║ ██║██╔══╝ ██╔══██╗
-            // ╚══╝ \n╚██████╔╝██║ ██║██║ ██║██████╔╝███████╗██║ ██║ ██╗ \n ╚═════╝ ╚═╝
-            // ╚═╝╚═╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═╝ ╚═╝ ");
-            // System.out.println(" ╚═════╝ ╚═╝ ╚═╝╚═╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═╝ ╚═╝");
+        // Options - Instructions, PLay, Exit
+        System.out.println("Enter the number indicated before the desired option:");
+        System.out.printf("%4d: %s\n", 1, "Instructions");
+        System.out.printf("%4d: %s\n", 2, "Play");
+        System.out.printf("%4d: %s\n", 0, "Return to the main menu at any point in the game.");
+        System.out.printf("%4d: %s\n", -1, "Quit at any point in the game.");
+        System.out.println();
 
-            // Options - Instructions, PLay, Exit
-            System.out.println("Enter the number indicated before the desired option:");
-            System.out.printf("%4d: %s\n", 1, "Instructions");
-            System.out.printf("%4d: %s\n", 2, "Play");
-            System.out.printf("%4d: %s\n", 0, "Return to the main menu at any point in the game.");
-            System.out.printf("%4d: %s\n", -1, "Quit at any point in the game.");
-            System.out.println();
-
-            // Collect input from the user and run the method based on their response.
-            input = in.nextLine();
-
-            if (input.equals("1")) {
-                instructions();
-                good = true;
-            } else if (input.equals("2")) {
-                game();
-                good = true;
-            } else if (input.equals("0"))
-                System.out.println("\nYou are already at the main menu!");
-            else if (input.equals("-1"))
+        // Collect input from the user and run the method based on their response.
+        try {
+            response = readInput();
+            if (response.equals("1")) {
+                instructionsScreen = true;
+            } else if (response.equals("2")) {
+                gameScreen = true;
+            } else if (response.equals("-1"))
                 quitProgram();
             else
                 System.out.println("\nInvalid option...");
-            System.out.println();
+        } catch (Exception e) {
+            System.out.println("\nYou are already at the main menu!");
         }
+        System.out.println();
     }
 
     // Method for displaying instructions
     public static void instructions() {
-        // Variable declaration
-        String input;
-        boolean good = false;
 
         // Display instructions
         clearScreen();
         printHeader("Instructions");
         System.out.println("hmhmhm");
-        System.out.println();
 
-        while (!good) {
-            // Return to menu message
-            System.out.println("To return to the main menu, enter 0.");
+        // Return to menu message
+        System.out.println("\nTo return to the main menu, enter 0.\n");
 
-            // Collect input from the user.
-            input = in.nextLine();
-
-            if (input.equals("0")) {
-                mainMenu();
-                good = true;
-            } else if (input.equals("-1"))
-                quitProgram();
-            else
-                System.out.println("\nInvalid option...");
-            System.out.println();
+        // Collect input from the user.
+        try {
+            response = readInput();
+            System.out.println("\nInvalid option...");
+        } catch (Exception e) {
+            System.out.println("Returning to main screen...");
         }
     }
 
     public static void game() {
         // Variable declaration
         String[][][] questions = {
-                { { "g1q", "g1a" }, { "g1q2", "g1a2" } }, // Grade 1
-                { { "g2q", "g2a" }, { "g2q2", "g2a2" } }, // Grade 2
-                { { "g3q", "g3a" }, { "g3q2", "g3a2" } }, // Grade 3
-                { { "g4q", "g4a" }, { "g4q2", "g4a2" } }, // Grade 4
-                { { "g5q", "g5a" }, { "g5q2", "g5a2" } }, // Grade 5
-                { { "fq", "fa" }, { "fq2", "fa2" } } // Final question
+                { { "g1q", "g1a", "g1c" }, { "g1q2", "g1a2", "g1c2" } }, // Grade 1
+                { { "g2q", "g2a", "g2c" }, { "g2q2", "g2a2", "g2c2" } }, // Grade 2
+                { { "g3q", "g3a", "g3c" }, { "g3q2", "g3a2", "g3c2" } }, // Grade 3
+                { { "g4q", "g4a", "g4c" }, { "g4q2", "g4a2", "g4c2" } }, // Grade 4
+                { { "g5q", "g5a", "g5c" }, { "g5q2", "g5a2", "g5c2" } }, // Grade 5
+                { { "fq", "fa", "fc" }, { "fq2", "fa2", "fc2" } } // Final question
         };
         int qNum = 1;
         int qRange = 2;
         boolean alive = true;
         boolean dropOut = false;
-        boolean peek = true;
-        boolean copy = true;
+        boolean peek = false;
+        boolean copy = false;
         boolean save = true;
+        boolean getInput = true;
         int money = 0;
+        int randQNum;
 
         String response = "";
         String q;
         String a;
-        String classmateAnswer = "think where to put later, in array or smt";
+        String classmateAnswer;
 
-        while (alive && qNum <= 10) {
-            int randQNum = (int) (qRange * Math.random());
+        while (gameScreen && alive && qNum <= 10) {
+            randQNum = (int) (qRange * Math.random());
             q = questions[qNum / 2][randQNum][0];
             a = questions[qNum / 2][randQNum][1];
+            classmateAnswer = questions[qNum / 2][randQNum][2];
+            getInput = true;
 
-            while (true) {
+            while (gameScreen && alive) {
                 clearScreen();
                 printHeader("Game");
                 printHeader("Question " + qNum);
-                System.out.println(q);
-                response = in.nextLine();
-
-                if (response.equalsIgnoreCase("peek")) {
-                    // peek
-                    if (!peek) {
-                        System.out.println("You peek at your classmate's answer: [tmp].");
-                        peek = true;
-                    }
-                    else {
-                        System.out.println("You already used your peek cheat!");
-                    }
-                } else if (response.equalsIgnoreCase("copy")) {
-                    // copy
-                    if (!copy) {
-                        System.out.println("You copy your classmate's answer: [tmp2].");
-                        response = classmateAnswer;
-                    }
-                    else {
-                        System.out.println("You already used your copy cheat!");
-                    }
-                } else if (response.equalsIgnoreCase(a)) {
-                    // correct answer
-                    System.out.println("Correct!");
-                } else {
-                    // incorrect answer
-                    if (save) {
-                        System.out.println("Incorrect! [use classmate answer]");
-                    }
-                    else {
-                        System.out.println();
+                System.out.println(q + "\n");
+                if (getInput) {
+                    try {
+                        response = readInput();
+                    } catch (Exception e) {
+                        System.out.println("Returning to main screen...");
+                        gameScreen = false;
+                        break;
                     }
                 }
-            }
 
+                if (response.equalsIgnoreCase("copy")) {
+                    // copy
+                    if (!copy) {
+                        System.out.println("\nYou copy your classmate's answer: " + classmateAnswer + ".");
+                        copy = true;
+                        response = classmateAnswer;
+                    } else {
+                        System.out.println("\nYou already used your copy cheat!");
+                    }
+                    wait(2000);
+                    getInput = false;
+                } else if (response.equalsIgnoreCase("peek")) {
+                    // peek
+                    if (!peek) {
+                        System.out.println(
+                                "\nYou peek at your classmate's answer: " + classmateAnswer + ". Your turn to answer!");
+                        peek = true;
+                    } else {
+                        System.out.println("\nYou already used your peek cheat!");
+                    }
+                    wait(2000);
+                } else if (response.equalsIgnoreCase("drop out")) {
+                    // drop out
+                    dropOut = true;
+                    alive = false;
+                } else if (!response.equalsIgnoreCase(a)) {
+
+                    // incorrect answer
+                    if (save) {
+                        save = false;
+                        System.out.print(
+                                "\nIncorrect! You have been saved with your classmates answer: " + classmateAnswer);
+                        response = classmateAnswer;
+                        if (!response.equalsIgnoreCase(a)) {
+                            System.out.println("... or not. ");
+                            alive = false;
+                        } else
+                            System.out.println("!");
+                        getInput = false;
+                    } else {
+                        System.out.println("\nIncorrect! Unfortunately, you already used your save cheat.");
+                        alive = false;
+                    }
+                    wait(2000);
+                } else {
+                    // correct answer
+                    System.out.println("\nCorrect!");
+                    qNum++;
+                    break;
+                }
+            }
         }
 
-        if (!alive) {
-            if (dropOut) {
-                clearScreen();
-                printHeader("Drop Out");
-                System.out.println("You dropped out of school... winning $" + money + "!");
+        if (gameScreen) {
+            if (!alive) {
+                if (dropOut) {
+                    clearScreen();
+                    printHeader("Drop Out");
+                    System.out.println("You dropped out of school... winning $" + money + "!");
+                } else {
+                    clearScreen();
+                    printHeader("Failure");
+                    System.out.println("You failed school... losing all hard-earned money.");
+                }
             } else {
-                clearScreen();
-                printHeader("Failure");
-                System.out.println("You failed school... losing all hard-earned money.");
+                printHeader("Winner");
+                System.out.println("You passed! Time for the final question...");
+                // final q
             }
-        } else {
-            printHeader("Winner");
-            System.out.println("You passed! Time for the final question...");
-            // final q
-        }
 
-        while (true) {
-            System.out.println("\n");
-            System.out.println("Look at the camera and answer: are you smarter than a 5th grader?");
-            response = in.nextLine();
-            if (response.equalsIgnoreCase("yes")) {
-                if (alive)
-                    System.out.println("Congratulations!");
-                else
-                    System.out.println("Liar!");
-                break;
-            } else if (response.equalsIgnoreCase("no")) {
-                if (!alive)
-                    System.out.println("Loser");
-                else
-                    System.out.println("Nerd");
-                break;
-            } else {
-                System.out.println("Invalid response. Try again: ");
+            while (true) {
+                System.out.println("\nLook at the camera and answer: are you smarter than a 5th grader?\n");
+                try {
+                    response = readInput();
+
+                    if (response.equalsIgnoreCase("yes")) {
+                        if (alive)
+                            System.out.println("\nCongratulations!");
+                        else
+                            System.out.println("\nLiar!");
+                        break;
+                    } else if (response.equalsIgnoreCase("no")) {
+                        if (!alive)
+                            System.out.println("\nLoser");
+                        else
+                            System.out.println("\nNerd");
+                        break;
+                    } else {
+                        System.out.println("\nInvalid response. Try again: ");
+                    }
+                } catch (Exception e) {
+                }
             }
+            System.out.println("\nPress 0 to return to the main menu.");
+            gameScreen = false;
         }
-        System.out.println("Press 0 to return to the main menu and -1 to exit the game.");
     }
 
     public static void main(String[] args) {
-        // Variable declaration
-
-        mainMenu();
+        while (true) {
+            if (instructionsScreen)
+                instructions();
+            else if (gameScreen)
+                game();
+            else
+                mainMenu();
+        }
     }
 }
