e882953b0f007d605f9c76a365a8b4afd71c8d5b
Grace Pu <33011813+Goldengold123@users.noreply.github.com>
2023-03-28 14:33:17 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+// https://github.com/fusesource/jansi
+// https://stackoverflow.com/questions/52767585/how-can-you-use-vt100-escape-codes-in-java-on-windows
+// https://learn.microsoft.com/en-us/windows/console/setconsolemode
+// https://superuser.com/questions/413073/windows-console-with-ansi-colors-handling
+// https://github.com/microsoft/WSL/issues/1173
+
+//import org.fusesource.jansi.Ansi.*;
+//import org.fusesource.jansi.Ansi.Color.*;
+
+public class test {
+    public static void main(String[] args) {
+        //System.out.println( ansi().eraseScreen().fg(RED).a("Hello").fg(GREEN).a(" World").reset() );
+    }
+}
